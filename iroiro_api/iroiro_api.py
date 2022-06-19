df97daa2d059d94b02482ac32a24953608dd8a77
Benjamin <benj.james.carter@gmail.com>
2022-06-19 12:37:03 -0700

    added basic endpoints and notes on use

--- iroiro_api/iroiro_api.py
+++ iroiro_api/iroiro_api.py
@@ -1,12 +1,43 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+"""
+This is an instance of the FastAPI class
+providing access to all methods of the FastAPI class
+"""
+
+"""
+Each of the HTTP methods is called an "operation"
+
+POST: to create data
+GET: to read data
+PUT: to update data
+DELETE: to delete data
+
+Example:
+Sending a GET request to 'http://127.0.0.1:8000/' will call the @app.get("/") root decorated method
+Sending a GET request to 'http://127.0.0.1:8000/login' will call the @app.get("/login") login method
+"""
 
 
+# Basic endpoints
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
   
 @app.get('/login')
 async def login():
-    return {"login page test!": "welcome to the login page"}
+    return {"login page": "welcome to the login page"}
+  
+@app.get('/gallery')
+async def gallery():
+    return {"the gallery" : "here you can see photos uploaded by all users"}
+  
+@app.get('/upload')
+async def upload():
+    return {"upload page": "Here you can upload new photos once logged in"}
+  
+@app.get('/mypage')
+async def mypage():
+    return {"mypage": "Here you view all photos you have uploaded"}
